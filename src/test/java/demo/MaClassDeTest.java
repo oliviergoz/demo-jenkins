bc24fe960d089100afb0591b944422d6476eefc8
olivier <o.gozlan@ajc-ingenierie.fr>
2022-12-21 17:15:07 +0100

    modification du projet

---
+++ src/test/java/demo/MaClassDeTest.java
@@ -0,0 +1,60 @@
+package demo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import demoJavaAvecGit.FactorielException;
+import demoJavaAvecGit.FonctionMathematique;
+
+class MaClassDeTest {
+
+	FonctionMathematique fct;
+	
+	@BeforeEach
+	void avantChaque() {
+		fct=new FonctionMathematique();
+	}
+	
+	@AfterEach
+	void apresChaqueTest() {
+		fct=null;
+	}
+	
+	@BeforeAll
+	static void avantLePremierTest() {
+		//execute 1 fois avant le premier test
+	}
+	
+	@AfterAll
+	static void apresLeDernierTest() {
+		//execute 1 fois apres le dernier test
+	}
+	
+	
+	@Test
+	void factorielTest() {
+		
+		assertEquals(6, fct.factoriel(3));
+	}
+	
+	@Test
+	void factorielExceptioTest() {
+		assertThrows(FactorielException.class,()->{
+			fct.factoriel(0);
+		});
+		
+	}
+
+	@Test
+	void additionTest() {
+		assertEquals(10, fct.addition(2, 8));
+		//assertEquals(15, fct.addition(10, 6));
+		assertEquals(15, fct.addition(10, 5));
+	}
+
+}
