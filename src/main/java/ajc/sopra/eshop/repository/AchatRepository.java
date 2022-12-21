2151f59f50192476b64ed3bfe4d897d50a2dd0e0
olivier <o.gozlan@ajc-ingenierie.fr>
2022-12-21 14:42:32 +0100

    update file

--- src/main/java/ajc/sopra/eshop/repository/AchatRepository.java
+++
@@ -1,9 +0,0 @@
-package ajc.sopra.eshop.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ajc.sopra.eshop.model.Achat;
-
-public interface AchatRepository extends JpaRepository<Achat, Integer>{
-
-}
--- src/main/java/ajc/sopra/eshop/repository/ClientRepository.java
+++
@@ -1,9 +0,0 @@
-package ajc.sopra.eshop.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ajc.sopra.eshop.model.Client;
-
-public interface ClientRepository extends JpaRepository<Client, Integer>{
-
-}
